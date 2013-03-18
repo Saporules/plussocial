9a0671a7c6f22bf3b944c53433c2a5e1bef10255
SapoRules <asaf_lopez@outlook.com>
2013-03-18 12:27:01 -0600

    Created a rails app and added status updates.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plussocial::Application.config.secret_token = '576dfa67ad25666aae61940876514bf2b4fb416bb447971b2ca077d384b5d5b29958fc0d9fd9cdca9099d076ea4be6077b263bd69128a95268d729bd550e7c3e'
